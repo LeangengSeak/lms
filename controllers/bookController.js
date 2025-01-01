de805a7840e8ba8dec7038a3b1828877ab81e8eb
LeangengSeak <leangeng03@gmail.com>
2025-01-01 23:34:46 +0700

    add validation, upload file

--- controllers/bookController.js
+++ controllers/bookController.js
@@ -1,6 +1,10 @@
-const bookQuery = require('../resources/bookQuries')
+const bookQuery = require('../resources/bookQuries');
 const categoryQuery = require('../resources/categoryQueries');
 const authorQuery = require('../resources/authorQueries');
+const fs = require('fs');
+const vBook = require('../validation/bookValidation');
+
+const DEFAULT_COVER = "default-cover.jpg";
 
 const getAll = async (req, res) => {
     try {
@@ -40,29 +44,68 @@ const getCreate = async (req, res) => {
 
 // Create a new book and redirect to the books list
 const postCreate = async (req, res) => {
-    const { bookName, author, category } = req.body;
-    const arrBook = [bookName, author, category];
+    const { error, value } = vBook(req.body);
+    if (error) {
+        return res.status(400).send(error.message);
+    }
+    const { bookName, author, category } = value;
+    let sampleFile, sampleFileName, uploadPath;
+    if (req.files) {
+        sampleFile = req.files.bookCover;
+        sampleFileName = Date.now() + sampleFile.name;
+        uploadPath = "./public/upload/" + sampleFileName;
+        sampleFile.mv(uploadPath, err => {
+            if (err) throw err;
+            console.log('successfully uploaded');
+        });
+    } else {
+        sampleFileName = DEFAULT_COVER;
+    }
+
+    const arrBook = [bookName, sampleFileName, author, category];
     try {
         await bookQuery.createBook(arrBook);
         console.log("Book created!");
         res.redirect("/books");
     } catch (err) {
-        console.log(err)
-        res.status(500).send('Internal Server Error')
+        console.log(err);
+        res.status(500).send('Internal Server Error');
     }
 };
 
 // Update an existing book and redirect to the books list
 const postEdit = async (req, res) => {
-    const { bookId, bookName, author, category } = req.body;
-    const arrBook = [bookName, author, category, bookId];
+    console.log(req.files);
+    console.log(req.body);
+    const { error, value } = vBook(req.body);
+    if (error) {
+        return res.status(400).send(error.message);
+    }
+    const { bookId, bookName, author, category } = value;
+    let file;
+    if (!req.files) {
+        file = req.body.oldCover;
+    } else {
+        const sampleFile = req.files.bookCover;
+        const sampleFileName = Date.now() + sampleFile.name;
+        const uploadPath = "./public/upload/" + sampleFileName;
+        sampleFile.mv(uploadPath, err => {
+            if (err) throw err;
+            console.log('successfully uploaded');
+            if (req.body.oldCover !== DEFAULT_COVER) {
+                fs.unlinkSync('public/upload/' + req.body.oldCover);
+            }
+        });
+        file = sampleFileName;
+    }
+    const arrBook = [bookName, file, author, category, bookId];
     try {
-        await bookQuery.updateBook(arrBook)
+        await bookQuery.updateBook(arrBook);
         console.log("Book updated!");
         res.redirect("/books");
     } catch (err) {
-        console.log(err)
-        res.status(500).send('Internal Server Error')
+        console.log(err);
+        res.status(500).send('Internal Server Error');
     }
 };
 
@@ -70,12 +113,16 @@ const postEdit = async (req, res) => {
 const deleteRecord = async (req, res) => {
     const { id } = req.params;
     try {
+        const bookResult = await bookQuery.getById(id);
+        if (bookResult.bookCover !== DEFAULT_COVER) {
+            fs.unlinkSync('public/upload/' + bookResult.bookCover);
+        }
         await bookQuery.deleteBook(id);
         console.log("Book deleted!");
         res.redirect("/books");
     } catch (err) {
-        console.log(err)
-        res.status(500).send('Internal Server Error')
+        console.log(err);
+        res.status(500).send('Internal Server Error');
     }
 };
 
