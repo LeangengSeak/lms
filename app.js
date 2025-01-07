7e1f60d783cce257c925b3fe73fc9e1807af0211
LeangengSeak <leangeng03@gmail.com>
2025-01-07 22:29:33 +0700

    add middleware and require new package

--- app.js
+++ app.js
@@ -1,43 +1,43 @@
-const express = require('express')
-const fileUpload = require('express-fileupload');
+const express = require("express");
+const fileUpload = require("express-fileupload");
 const app = express();
 
-const authRoutes = require('./routes/authRoutes')
-const bookRoutes = require('./routes/bookRoutes');
-const authorRoutes = require('./routes/authorRoutes')
-const categoryRoutes = require('./routes/categoryRoutes')
-const notFound = require('./middleware/not-found')
+const authRoutes = require("./routes/authRoutes");
+const bookRoutes = require("./routes/bookRoutes");
+const authorRoutes = require("./routes/authorRoutes");
+const categoryRoutes = require("./routes/categoryRoutes");
+const notFound = require("./middleware/not-found");
+const cookieParser = require("cookie-parser");
+const { requireAuth, checkUser } = require("./middleware/authMiddleware");
 
 // Set view engine
-app.set('view engine', 'ejs')
+app.set("view engine", "ejs");
 
 // Middleware
-app.use(express.static('public'))
-app.use(fileUpload())
-app.use(express.urlencoded({ extended: true }))
+app.use(express.static("public"));
+app.use(fileUpload());
+app.use(express.urlencoded({ extended: true }));
+app.use(cookieParser());
 
-// Routes
-app.get('/', (req, res) => {
-    res.render('index')
-})
+app.use("/auths", authRoutes);
+app.use("*", checkUser);
+app.get("/", requireAuth, (req, res) => {
+  res.render("index");
+});
+app.get("/blank", requireAuth, (req, res) => {
+  res.render("blank");
+});
 
-app.get('/blank', (req, res) => {
-    res.render('blank')
-})
-
-app.get('/tables', (req, res) => {
-    res.render('tables')
-})
-
-app.use('/auths', authRoutes)
-app.use('/books', bookRoutes)
-app.use('/authors', authorRoutes)
-app.use('/categories', categoryRoutes)
+app.get("/tables", requireAuth, (req, res) => {
+  res.render("tables");
+});
+app.use("/books", bookRoutes);
+app.use("/authors", authorRoutes);
+app.use("/categories", categoryRoutes);
 // 404 handler
-app.use(notFound)
-
+app.use(notFound);
 
 // Start server
-app.listen(5000, err => {
-    err ? console.log(err) : console.log('Server is running on port 5000');
-})
+app.listen(5000, (err) => {
+  err ? console.log(err) : console.log("Server is running on port 5000");
+});
